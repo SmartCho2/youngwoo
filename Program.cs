24424caf570be8836c439fb6c4d2e5d5e6f3fe0b
user's_name <user's_email>
2018-10-18 11:15:56 +0900

    the first version

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace helloworld
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("helloworld 이름");
+                System.Console.ReadKey();
+                return;
+            }
+
+            System.Console.WriteLine("hello,{0}", args[0]);
+            System.Console.ReadKey(); /* 콘솔창에서 키를 입력받기위해 대기 */
+       
+        }
+      
+            
+    
+     }
+}
